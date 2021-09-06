637110f99131f37897f3896231b19ef39fff35e9
Nestor Matias <solomatias321@gmail.com>
2021-09-06 10:16:55 +0200

    creating seeding data for db connection

--- Data/PrepDb.cs
+++ Data/PrepDb.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
@@ -19,7 +20,13 @@ namespace PlatformService.Data
         {
             if(!context.Platforms.Any())
             {
+                Console.WriteLine("--> Seeding Data ..");
 
+                context.Platforms.AddRange(
+                    new Platform(){ Name = "Dot net", Publisher = "Microsoft", Cost = "Free"},
+                    new Platform(){ Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free"},
+                    new Platform(){ Name = "Kubernetes", Publisher = "Cloud native computing", Cost = "Free"}
+                );
             }
             else{
                 Console.WriteLine("--> We already have data");
