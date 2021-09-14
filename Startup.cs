dd1a3521517d5a24f5bfcfcbd79cb65c79070dd7
Nestor Matias <solomatias321@gmail.com>
2021-09-14 11:05:54 +0200

    running migrations on local first and setting EF and pushing to docker hub

--- Startup.cs
+++ Startup.cs
@@ -19,16 +19,29 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            // if (_env.IsProduction())
+            // {
+                Console.WriteLine("--> Using SqlServer Db");
+                services.AddDbContext<AppDbContext>(opt =>
+                    opt.UseSqlServer(Configuration
+                    .GetConnectionString("PlatformsConn")));
+
+            // }else{
+            //     Console.WriteLine("--> Using InMEM Db");
+            //     services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            // }
+            
             
             services.AddScoped<IPlatformRepository, PlatformRepository>();
 
@@ -55,7 +68,7 @@ namespace PlatformService
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
@@ -66,7 +79,7 @@ namespace PlatformService
                 endpoints.MapControllers();
             });
 
-            PrepDb.PrepPopulation(app); 
+           // PrepDb.PrepPopulation(app, env.IsProduction()); 
         }
     }
 }
